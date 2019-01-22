9d6b52dc01ff5774e5f912d362b110c9646b9c71
vavnilov <vadim.avnilov@gmail.com>
2019-01-22 14:55:24 -0500

    Add Store functionality added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
+const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/', (req, res) => {
-  res.render('hello', {
-    name: "Vadim",
-    lastName: "Avnilov"
-  });
-});
+router.get('/', storeController.homePage)
+router.get('/add', storeController.addStore)
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
