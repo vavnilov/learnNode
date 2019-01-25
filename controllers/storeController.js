53195281794c83cadf2c3ec6ea0dcca235e47a11
vavnilov <vadim.avnilov@gmail.com>
2019-01-25 17:30:47 -0500

    geolocation added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -36,6 +36,8 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+  //set the location to be a point
+  req.body.location.type = 'Point'
   // find and update the store
   const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
     new: true,
